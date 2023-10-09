af62fab9c905d3250d46dff42d2fd735d60af9fd
afialisa <afianourish72@gmail.com>
2023-10-09 08:46:34 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program that prints out negative and positive numbers
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+        int n;
+	int m;
+	srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        m = n % 10;
+	if (m > 5)
+	{
+		printf("Last digit of %d and is greater than 5", n, m);
+	}
+	else if (m == 0)
+	{	
+		printf("Last digit of %d and is zero", n, m);
+	}
+	else if (m < 6 && m != 0)
+	{
+		printf("Last digit of %d and is less than 6 and not zero 0", n, m);
+	}
+	printf("\n");
+	return (0);
+}
