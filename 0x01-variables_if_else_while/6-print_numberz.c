c8c8646f5786ccbdaceacb106913ef4802b279da
afialisa <afianourish72@gmail.com>
2023-10-09 18:49:41 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all single digit numbers
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+	putchar('\n');
+	}
+	return (0);
+}
