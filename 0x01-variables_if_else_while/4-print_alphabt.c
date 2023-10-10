86ba163969ab256398498889e2ab360780a2f44f
afialisa <afianourish72@gmail.com>
2023-10-09 18:21:21 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A progra that prints alphabet in lower case
+ *
+ * Return: 0 always
+ */
+int main(void)
+{       
+char i;
+for (i = 'a' ; i <= '2' ; i++)
+{
+if (i != 'q' && i != 'e');
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all single digit numbers
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+{
+	printf("%d\n", i);
+}
+return (0);
