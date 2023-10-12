7c1e69165e635a9a724199e769e869fda3260a8f
afialisa <afianourish72@gmail.com>
2023-10-11 17:48:26 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,13 +6,14 @@
  *
  * Return: 1 for lowercase character or 0 for anything else
  */
-
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-	return (1);
-	}	
+		return (1);
+	}
 	else
-	return (0);
+	{
+		return (0);
+	}
 }
