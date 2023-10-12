65ad5d1839d9ee2f5ca7b16ecdc68fe32d668662
afialisa <afianourish72@gmail.com>
2023-10-11 17:39:09 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,17 +2,18 @@
 
 /**
  * _isalpha - Checks for alphabetic character
- * @c: The character to be checked 
+ * @c: The character to be checked
  *
  * Return: 1 for alphabetic character or 0 for anything else
  */
-
 int _isalpha(int c)
 {
-if ((c>= 65 && c <= 97) || (c >= 97 && c <= 122))
-{
-return (1);
-}
-else
-return (0);
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
