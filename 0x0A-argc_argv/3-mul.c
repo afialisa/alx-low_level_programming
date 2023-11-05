a8d1d6429b1a56993bc4924fd0e4e4eff441f166
afialisa <afianourish72@gmail.com>
2023-11-05 04:28:52 -0800

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - A function that multiplies two numbers
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int a, b, answer;
+if (argc < 3 || argc > 3)
+{
+printf("Null\n");
+return (1);
+}
+a = _atoi(argv[1]);
+b = _atoi(argv[2]);
+answer = a *b;
+printf("%d\n", answer);
+return (0);
+}
