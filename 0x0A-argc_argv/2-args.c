44406ddeef259f234c37f81e35a1d6eccdd7ee36
afialisa <afianourish72@gmail.com>
2023-11-05 02:46:51 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - A function that prints all arguments it receives
+ * @argc: argument counts
+ * @argv: argument array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int a;
+for (a = 0; a < argc; a++)
+{
+printf("%s\n", argv[a]);
+}
+return (0);
+}
