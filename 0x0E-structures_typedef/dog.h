7c2356bf3a7699350531dc3172d8d10c20c845de
afialisa <afianourish72@gmail.com>
2023-11-06 08:31:50 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog's information
+ * @name: pet name if dog
+ * @age: the dog's age
+ * @owner: the one who owns the dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char owner;
+}
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+#endif
