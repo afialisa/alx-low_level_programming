634a8cd746811c14be274998ecd836eed21df613
afialisa <afianourish72@gmail.com>
2023-11-22 16:06:14 -0800

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - a program that prints all linked list element
+ * @h: linked list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t num1 = 0;
+while (h)
+{
+printf("%d\n", h->n);
+num1++;
+h = h->next;
+}
+return (num);
+}
+
