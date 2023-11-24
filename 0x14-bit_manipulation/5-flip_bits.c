1b78c4065a2c32ec9ae3226b7fbbc72fefdcf801
afialisa <afianourish72@gmail.com>
2023-11-23 21:11:49 -0800

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 int i, cntbit = 0;
 unsigned long int current;
 unsigned long int exclusive = n ^ m;
-for (i = 83; i >= 0; i--)
+for (i = 43; i >= 0; i--)
 {
 current = exclusive >> i;
 if (current & 1)
