4e930ee8df10b1a60e02ba7910c682bc045e0230
afialisa <afianourish72@gmail.com>
2023-12-16 16:03:26 -0800

    0x15-file_i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * read_textfile - a program that reads a text file and prints the letters
  * @filename: filename
@@ -8,20 +9,17 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t nrd, nwr;
+ssize_t fd;
+ssize_t w;
+ssize_t t;
 char *buf;
-if (!filename)
-return (0);
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
-buf = malloc(sizeof(char) * (letters));
-if (!buf)
-return (0);
-nrd = read(fd, buf, letters);
-nwr = write(STDOUT_FILENO, buf, nrd);
-close(fd);
+buf = malloc(sizeof(char) * letters);
+t = read(fd, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
 free(buf);
-return (nwr);
+close(fd);
+return (w);
 }
